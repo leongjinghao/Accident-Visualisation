7b35309848b175960aaa116b43911f1b67161228
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2022-03-07 20:11:12 +0800

    Update data_cleansing.py

--- data_cleansing.py
+++ data_cleansing.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
+# read csv dataset
 accident_data = pd.read_csv("US_Accidents_Dec20_updated.csv")
+
+# for generating new PK and FK
+rowIndex = 1
 
 # test
 print(accident_data[accident_data["ID"] == "A-3265557"])

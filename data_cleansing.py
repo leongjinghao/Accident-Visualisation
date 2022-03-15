6d1ecf45d0208ece744fa1da3cef3618675b73de
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2022-03-15 21:21:00 +0800

    Check if accident start or end time is null
    If row for accident start or end time is null, skip the particular row.

--- data_cleansing.py
+++ data_cleansing.py
@@ -5,22 +5,29 @@ accident_data = pd.read_csv("US_Accidents_Dec20_updated.csv")
 
 # Accident dataframe
 # TODO: to include fk fields programmatically
-accident = accident_data[["ID", "Severity", "Start_Time", "End_Time", "Description"]]
+accident = accident_data[
+    ["ID", "Severity", "Start_Time", "End_Time", "Description"]]
 
 # Accident Location dataframe
 # TODO: to include fk fields programmatically
-accident_location = accident_data[["ID", "Start_Lat", "Start_Lng", "End_Lat", "End_Lng", "Distance(mi)"]]
+accident_location = accident_data[
+    ["ID", "Start_Lat", "Start_Lng", "End_Lat", "End_Lng", "Distance(mi)"]]
 
 # Address dataframe
 # TODO: to include pk & fk fields programmatically
-address = accident_data[["Number", "Street", "Side", "City", "County", "State", "Zipcode", "Country", "Timezone", "Airport_Code", ]]
+address = accident_data[
+    ["Number", "Street", "Side", "City", "County", "State", "Zipcode", "Country", "Timezone", "Airport_Code", ]]
 
 # Weather dataframe
 # TODO: to include pk & fk fields programmatically
-weather = accident_data[["Weather_Timestamp", "Temperature(F)", "Wind_Chill(F)", "Humidity(%)", "Pressure(in)", "Visibility(mi)", "Wind_Direction", "Wind_Speed(mph)", "Precipitation(in)", "Weather_Condition"]]
+weather = accident_data[
+    ["Weather_Timestamp", "Temperature(F)", "Wind_Chill(F)", "Humidity(%)", "Pressure(in)", "Visibility(mi)",
+     "Wind_Direction", "Wind_Speed(mph)", "Precipitation(in)", "Weather_Condition"]]
 
 # Location Property dataframe
-location_property = accident_data[["Amenity", "Bump", "Crossing", "Give_Way", "Junction", "No_Exit", "Railway", "Roundabout", "Station", "Stop", "Traffic_Calming", "Traffic_Signal", "Turning_Loop"]]
+location_property = accident_data[
+    ["Amenity", "Bump", "Crossing", "Give_Way", "Junction", "No_Exit", "Railway", "Roundabout", "Station", "Stop",
+     "Traffic_Calming", "Traffic_Signal", "Turning_Loop"]]
 
 # for generating new PK and FK
 rowIndex = 1
@@ -32,9 +39,14 @@ rowIndex = 1
 # TODO: replace value 10 to len(accident_data.index) for actual execution
 for rowIndex in range(10):
     # testing: print out all tuples
-    print(accident_data.iloc[[rowIndex]])
+    # print(accident_data.iloc[[rowIndex]])
 
+    ''' Data Cleansing/Validation Process '''
     # TODO: data cleansing for Accident dataframe
+    # Skip row if Start_Time or End_Time is null
+    if accident.iloc[[rowIndex]]["Start_Time"].isnull().values.any() or \
+            accident.iloc[[rowIndex]]["End_Time"].isnull().values.any():
+        continue
 
     # TODO: data cleansing for Accident Location dataframe
 
@@ -43,3 +55,6 @@ for rowIndex in range(10):
     # TODO: data cleansing for Weather dataframe
 
     # TODO: data cleansing for Location Property dataframe
+
+    ''' Storage of Cleansed/Validated data '''
+    # TODO: append cleansed/validated row into respective csv file

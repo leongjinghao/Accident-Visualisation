bfbcf332f95834194c5e2a2bb322185808728e21
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2022-03-16 19:31:35 +0800

    Update data_cleansing.py

--- data_cleansing.py
+++ data_cleansing.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import os
 
 # read csv dataset
-accident_data = pd.read_csv("US_Accidents_Dec20_updated.csv")
+# TODO: to remove nrows=10 in final execution
+accident_data = pd.read_csv("US_Accidents_Dec20_updated.csv", nrows=10)
 
 # Accident dataframe
 # TODO: to include fk fields programmatically
@@ -29,32 +31,81 @@ location_property = accident_data[
     ["Amenity", "Bump", "Crossing", "Give_Way", "Junction", "No_Exit", "Railway", "Roundabout", "Station", "Stop",
      "Traffic_Calming", "Traffic_Signal", "Turning_Loop"]]
 
-# for generating new PK and FK
-rowIndex = 1
-
 # test
 # print(accident_data[accident_data["ID"] == "A-3265557"])
 
-# traverse all tuples
-# TODO: replace value 10 to len(accident_data.index) for actual execution
-for rowIndex in range(10):
-    # testing: print out all tuples
-    # print(accident_data.iloc[[rowIndex]])
+# Function for appending row to csv file
+def appendRowToCSV(file, dataframe, index):
+    with open(file, 'a+') as f:
+        # If file is empty, write in the header
+        f.seek(0)
+        if f.read() == "":
+            f.write(','.join(dataframe.columns.values.tolist()))
+            f.write('\n')
+        f.write(''.join(
+            dataframe.iloc[[index]].to_csv(
+                header=False,
+                index=False,
+                sep=','
+            ).strip('\n').split('\n')
+        ))
 
-    ''' Data Cleansing/Validation Process '''
-    # TODO: data cleansing for Accident dataframe
-    # Skip row if Start_Time or End_Time is null
-    if accident.iloc[[rowIndex]]["Start_Time"].isnull().values.any() or \
-            accident.iloc[[rowIndex]]["End_Time"].isnull().values.any():
-        continue
+# Function for deleting all existing individual csv files
+def deleteExistingCSV():
+    os.remove('accident.csv')
+    os.remove('accident_location.csv')
+    os.remove('address.csv')
+    os.remove('weather.csv')
+    os.remove('location_property.csv')
 
-    # TODO: data cleansing for Accident Location dataframe
+def main():
+    # remove all existing individual csv files
+    deleteExistingCSV()
 
-    # TODO: data cleansing for Address dataframe
+    '''
+    1. Data Cleansing/Validation Process:
+    To traverse all tuples 
+    '''
+    # TODO: replace value 10 to len(accident_data.index) for actual execution
+    for rowIndex in range(len(accident_data.index)):
+        # testing: print out all tuples
+        # print(accident_data.iloc[[rowIndex]])
 
-    # TODO: data cleansing for Weather dataframe
+        # TODO: data cleansing for Accident dataframe
+        # Skip row if Start_Time or End_Time is null
+        if accident.iloc[[rowIndex]]["Start_Time"].isnull().values.any() or \
+                accident.iloc[[rowIndex]]["End_Time"].isnull().values.any():
+            continue
 
-    # TODO: data cleansing for Location Property dataframe
+        # TODO: data cleansing for Accident Location dataframe
 
-    ''' Storage of Cleansed/Validated data '''
-    # TODO: append cleansed/validated row into respective csv file
+        # TODO: data cleansing for Address dataframe
+
+        # TODO: data cleansing for Weather
+        if weather.iloc[[rowIndex]]["Weather_Condition"].isnull().values.any():
+            continue
+
+        # TODO: data cleansing for Location Property dataframe
+
+        ''' 
+        2. Storage of Cleansed/Validated data 
+        Append cleansed/validated row into respective csv file
+        '''
+        # Append row to accident.csv
+        appendRowToCSV('accident.csv', accident, rowIndex)
+
+        # Append row to accident_location.csv
+        appendRowToCSV('accident_location.csv', accident_location, rowIndex)
+
+        # Append row to address.csv
+        appendRowToCSV('address.csv', address, rowIndex)
+
+        # Append row to weather.csv
+        appendRowToCSV('weather.csv', weather, rowIndex)
+
+        # Append row to location_property.csv
+        appendRowToCSV('location_property.csv', location_property, rowIndex)
+
+
+if __name__ == "__main__":
+    main()
